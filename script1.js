96f7a32fbbb5afd09db2197b5c1fee9c25a4a690
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-12-10 23:13:06 +0200

    "while" branch

--- script1.js
+++ script1.js
@@ -6,61 +6,74 @@ while (i < arr.length) {
     if (arr[i] === 12) {
         num12++;
     }
+    i++;
 }
 console.log("Кількість числа 12:" + num12);//количество числа 12
 
 
 
 let negativeNumbers = 0;
+i = 0;
 while (i < arr.length) {
     if (arr[i] < 0) {
         negativeNumbers++;
     }
+    i++;
 }
 console.log("Негативних чисел всього:" + negativeNumbers);//посчитай количество отрицательных чисел
 
 let positiveNumbers = 0;
+i = 0;
 while (i < arr.length)  {
     if (arr[i] > 0) {
         positiveNumbers++;
     }
+    i++;
 }
 console.log("Позитивних чисел всього:" + positiveNumbers);//посчитай количество положительных чисел
 
 
 let evenNumbers = 0;
+i = 0;
 while (i < arr.length) {
     if (arr[i] % 2 === 0) {
         evenNumbers++;
     }
+    i++;
 }
 console.log("Кількість парних чисел:"+ evenNumbers);//посчитай количество четных чисел 
 
 
 let oddNumbers = 0;
+i = 0;
 while (i < arr.length)  {
     if (arr[i] % 2 !== 0) {
         oddNumbers++;  
     }
+    i++;
 }
 console.log("Кількість непарних чисел:"+ oddNumbers);//посчитай количество нечетных чисел
 
 let sum = 0;
+i = 0;
 while (i < arr.length)  {
     if(arr[i] > 0) {
         sum = sum + arr[i];
     }
+    i++;
 }
 console.log("Сума позитних чисел:" + sum); //сложи все положительные числа 
 
 
 let division3 = 0;
+i = 0;
 while (i < arr.length) {
     {
         if (arr[i] % 3 === 0) {
             console.log(" діляться на 3: " + arr[i]);
             division3++;
         }
+        i++;
     }
     console.log("Чисел, які діляться на 3 всього:" + division3);// вывети в консоль все числа, которые делятся на 3
 }
